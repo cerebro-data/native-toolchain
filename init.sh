8427b5294e021b08c6a8dc9f09c106ac0e0ae0ce
Joe Buck <joe@okera.com>
2019-12-05 10:18:49 -0800

    [binutils] Upgrading to 2.32
    Builds on 16.04 with a more recent kernel were failing because
    binutils required linux kernel 2.6 symbols and those are absent.
    
    Upgrading to 2.32 in the hopes that this resolves the issue.

--- init.sh
+++ init.sh
@@ -77,7 +77,7 @@ export CLEAN
 : ${CLEAN_TMP_AFTER_BUILD=0}
 export CLEAN_TMP_AFTER_BUILD
 
-: ${BINUTILS_VERSION=2.28.1}
+: ${BINUTILS_VERSION=2.32}
 export BINUTILS_VERSION
 
 # Flag to determine the system compiler is used
