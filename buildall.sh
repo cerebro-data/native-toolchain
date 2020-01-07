785b97375664944628461051d5760cc9a7ede9aa
Joe Buck <joe@okera.com>
2020-01-07 20:31:28 +0000

    [thrift] Use 0.9.0-p4 as the historical version, not 0.9.3-p4
    We never really upgraded to 0.9.3 and are instead using 0.9.0-p4
    as our historical version.

--- buildall.sh
+++ buildall.sh
@@ -95,8 +95,8 @@ export BISON_VERSION=3.0.4
 
 if [[ ! "$OSTYPE" == "darwin"* ]]; then
   if (( BUILD_HISTORICAL )); then
-    echo "Building thrift 0.9.3"
-    THRIFT_VERSION=0.9.3-p4 $SOURCE_DIR/source/thrift/build.sh
+    echo "Building thrift 0.9.0"
+    THRIFT_VERSION=0.9.0-p4 $SOURCE_DIR/source/thrift/build.sh
     echo "Building thrift 0.11.0-p2"
     THRIFT_VERSION=0.11.0-p2 $SOURCE_DIR/source/thrift/build.sh
   fi
